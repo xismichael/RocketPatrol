9b89cf7d9d3942276f900565912c068f69ca414a
Michael Xi <michael@Michaels-MacBook-Air-3.local>
2025-01-20 16:59:15 -0800

    trying to fix photo not loading when using github deplyment

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,9 +4,9 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('rocket', '../../assets/rocket.png')
-        this.load.image('spaceship', '../../assets/spaceship.png')
-        this.load.image('starfield', '../../assets/starfield.png')
+        this.load.image('rocket', './assets/rocket.png')
+        this.load.image('spaceship', './assets/spaceship.png')
+        this.load.image('starfield', './assets/starfield.png')
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {
             frameWidth: 64,
